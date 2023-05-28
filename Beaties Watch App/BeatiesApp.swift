bc104369022273a6d0558b4cd61ff1685647acb6
Geoff Pado <geoff@pado.name>
2023-05-28 12:11:37 -0700

    Initial Commit

---
+++ Beaties Watch App/BeatiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatiesApp.swift
+//  Beaties Watch App
+//
+//  Created by Geoff Pado on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Beaties_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
